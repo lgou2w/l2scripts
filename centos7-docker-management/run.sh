b5a506c2df5670d500db7fcb15eafa03258aaa97
ShaojieHe <741185493@qq.com>
2019-07-11 12:44:18 +0800

    add volume management

--- centos7-docker-management/run.sh
+++ centos7-docker-management/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 export counainer_count
-counainer_count=(`docker container ls | awk '{print $1}' | wc -l`)
-counainer_count=$(($counainer_count-1))
-check_docker_daemon(){
-    docker image list > /dev/null
-#check exit code if = 0 in order to see docker daemon if reachable
+counainer_count=($(docker container ls | awk '{print $1}' | wc -l))
+counainer_count=$(($counainer_count - 1))
+check_docker_daemon() {
+    docker image list >/dev/null
+    #check exit code if = 0 in order to see docker daemon if reachable
     if [[ $? != 0 ]]; then
         echo "连接不上docker daemon..."
         echo "可能是权限不够/或者是没有进入docker组"
@@ -13,7 +13,7 @@ check_docker_daemon(){
     else
         echo "docker daemon连接成功!"
     fi
-    
+
 }
 docker_list() {
     # checking docker
@@ -54,7 +54,7 @@ docker_image_management() {
     rm_RE)
         docker image list
         read -p "此选项用来以正则表达式删除镜像 请确认自己删除之前的镜像名" RE
-        del_image=`docker image ls | grep -Eo "$RE" - | awk '{print $1}'`
+        del_image=$(docker image ls | grep -Eo "$RE" - | awk '{print $1}')
         echo $del_image
         read -p "请确认想删除的镜像y/N" choice
         if [[ ${choice} == "y" ]]; then
@@ -120,15 +120,15 @@ docker_run() {
     echo "其实可以选用RE来启动容器的y/N"
     read method_RE
     if [[ -z ${method_RE} ]]; then
-            method_RE="N"
+        method_RE="N"
     fi
     if [[ ${method_RE} == "N" || ${method_RE} == "n" ]]; then
         echo "请输入你打算启动的容器/如果不存在会自动pull"
         read image
     elif [ ${method_RE} == "y" ]; then
         read -p "请输入你打算使用的正则表达式" RE
-        image=`docker image ls | grep -Eo "$RE" - | awk '{print $1}'`
-        count=`docker image ls | grep -Eo "$RE" - | awk '{print $1}' | wc -l`
+        image=$(docker image ls | grep -Eo "$RE" - | awk '{print $1}')
+        count=$(docker image ls | grep -Eo "$RE" - | awk '{print $1}' | wc -l)
         echo "这是你打算启动的镜像"
         echo ${image}
     fi
@@ -136,7 +136,7 @@ docker_run() {
     echo "简单启动ez(默认ez)"
     read method
     if [[ -z ${method} ]]; then
-            method="ez"
+        method="ez"
     fi
     case "${method}" in
     ez)
@@ -161,30 +161,28 @@ docker_run() {
         fi
         echo "正在用你设定的方法启动容器"
         if [[ ${method_RE} == "y" ]]; then
-            array_image=( "${image//,/}" )
-            for((;count>=0;))
-                do
-                    docker run ${launch_opition} ${expose_port} ${container_name} ${array_image[$count]} ${docker_command}
-                    count=$(($count-1))
-                done
+            array_image=("${image//,/}")
+            for (( ; count >= 0; )); do
+                docker run ${launch_opition} ${expose_port} ${container_name} ${array_image[$count]} ${docker_command}
+                count=$(($count - 1))
+            done
         fi
         echo ${array_image[*]}
-        array_image=( "${image//,/}" )
+        array_image=("${image//,/}")
         count=${#array_image[*]}
-            for((;count>=0;))
-                do
-                    docker run ${launch_opition} ${expose_port} ${container_name} ${array_image[$count]} ${docker_command}
-                    count=$(($count-1))
-                done
+        for (( ; count >= 0; )); do
+            docker run ${launch_opition} ${expose_port} ${container_name} ${array_image[$count]} ${docker_command}
+            count=$(($count - 1))
+        done
         docker ps
         ;;
-        *)
+    *)
         echo "你在选什么( 正在返回至主菜单"
         main_menu
         ;;
     esac
 }
-docker_network_management(){
+docker_network_management() {
     docker network list
     local method
     local network_name
@@ -194,46 +192,48 @@ docker_network_management(){
     read method
     case "${method}" in
     cre)
-    read -p "你打算创建网络的名字" network_name
-    docker network create --attachable true ${network_name}
-    docker_network_management
-    ;;
+        read -p "你打算创建网络的名字" network_name
+        docker network create --attachable true ${network_name}
+        docker_network_management
+        ;;
     co)
-    docker container list
-    read -p "要连接到网络的容器是" container_name
-    read -p "网络是" network_name
-    docker network connect ${network_name} ${container_name}
-    ;;
+        docker container list
+        read -p "要连接到网络的容器是" container_name
+        read -p "网络是" network_name
+        docker network connect ${network_name} ${container_name}
+        docker_network_management
+        ;;
     dco)
-    docker network list
-    read -p "网络是" network_name
-    docker network inspect ${network_name}
-    read -p "想要断开的容器是" container_name
-    docker network disconnect ${network_name} ${container_name}
-    docker_network_management
-    ;;
+        docker network list
+        read -p "网络是" network_name
+        docker network inspect ${network_name}
+        read -p "想要断开的容器是" container_name
+        docker network disconnect ${network_name} ${container_name}
+        docker_network_management
+        ;;
     ins)
-    read -p "你打算检查的网络是" network_name
-    docker network inspect ${network_name}
-    docker_network_management
-    ;;
+        read -p "你打算检查的网络是" network_name
+        docker network inspect ${network_name}
+        docker_network_management
+        ;;
     ls)
-    docker network ls
-    docker_network_management
-    ;;
+        docker network ls
+        docker_network_management
+        ;;
     rm)
-    docker network ls
-    read -p "你打算删除的网络是(用空格分割" name
-    docker network rm NETWORK ${name}
-    ;;
+        docker network ls
+        read -p "你打算删除的网络是(用空格分割" name
+        docker network rm NETWORK ${name}
+        docker_network_management
+        ;;
     *)
-    echo "正在返回至主菜单"
-    main_menu
-    ;;
-esac
+        echo "正在返回至主菜单"
+        main_menu
+        ;;
+    esac
 }
-install_docker(){
-    if ! command -v docker > /dev/null; then
+install_docker() {
+    if ! command -v docker >/dev/null; then
         yum install -y yum-utils
         yum-config-manager add-repo https://mirrors.ustc.edu.cn/docker-ce/linux/centos/docker-ce.repo
         yum install -y docker
@@ -248,7 +248,51 @@ install_docker(){
         main_menu
     fi
 }
-main_menu(){
+docker_volume_management() {
+    local choice
+    local name
+    local label
+    docker volume ls
+    echo "cr(创建) ins(详细信息) ls(列出) pr(清理) rm(删除)"
+    read -p "命名卷管理" choice
+    case "${choice}" in
+    cr)
+        read -p "你打算创建卷的名字" name
+        docker volume create ${name}
+        docker_volume_management
+        ;;
+    ins)
+        read -p "你打算检视卷的名字" name
+        docker volume inspect ${name}
+        docker_volume_management
+        ;;
+    ls)
+        docker volume ls
+        docker_volume_management
+        ;;
+    pr)
+        read -p "可选用清理的label" name
+        if [[ -z ${name} ]]; then
+            docker volume prume
+        else
+            read -p "你打算清除的label是" label
+            docker volume prume --filter value==${label}
+        fi
+        docker_volume_management
+        ;;
+    rm)
+        read -p "你打算删除的命名卷是" name
+        docker volume rm ${name}
+        docker_volume_management
+        ;;
+    *)
+        echo "无效选项( 正在返回至主菜单"
+        main_menu
+        ;;
+    esac
+
+}
+main_menu() {
     local choice_menu
     echo "docker manage script"
     echo "    1.镜像管理
@@ -257,35 +301,39 @@ main_menu(){
     4.启动容器
     5.容器网络管理
     6.列出容器/镜像
-    7.安装docker
+    7.管理卷
+    8.安装docker
     目前有${counainer_count}个容器在线"
     read -p "君欲何为? " choice_menu
     case "${choice_menu}" in
     1)
-    docker_image_management
-    ;;
+        docker_image_management
+        ;;
     2)
-    echo "TODO"
-    ;;
+        docker_container_management
+        ;;
     3)
-    control_docker
-    ;;
+        control_docker
+        ;;
     4)
-    docker_run
-    ;;
+        docker_run
+        ;;
     5)
-    docker_network_management
-    ;;
+        docker_network_management
+        ;;
     6)
-    docker_list
-    ;;
+        docker_list
+        ;;
     7)
-    install_docker
-    ;;
+        docker_volume_management
+        ;;
+    8)
+        install_docker
+        ;;
     *)
-    echo "? 正在退出" && exit 1
-    ;;
-esac
+        echo "? 正在退出" && exit 1
+        ;;
+    esac
 }
 check_docker_daemon
 main_menu
